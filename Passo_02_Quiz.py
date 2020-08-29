4e6db85ea5201c168972b7e76cb8710f49cadfda
daiana sousa <daianasousa04121996@gmail.com>
2020-08-28 21:56:24 -0300

    Criei o arquivo Passo_02_Quiz.py

---
+++ Passo_02_Quiz.py
@@ -0,0 +1,11 @@
+print('No Python, como se chama uma "caixa" usada para armaxenar dados?')
+resposta = input().lower()
+
+if resposta == 'variavel' or resposta == 'variável':
+  print(" :) " * 100)
+
+else: 
+  print(" :( " * 100)
+
+print("Obrigado por jogar!")
+
