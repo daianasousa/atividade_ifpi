577a9e3edd34718cb829818c369e989ed8085f85
daiana sousa <daianasousa04121996@gmail.com>
2020-08-30 23:45:11 -0300

    Criei o arquivo semana_03_3.py

---
+++ semana_03_3.py
@@ -0,0 +1,6 @@
+caractere = str(input('Escreva um caractere da sua escolha: ')).lower()
+  
+if caractere in ('b', 'c', 'd', 'f', 'g', 'h', 'j','k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'):
+  print(True) 
+else:
+  print(False)
