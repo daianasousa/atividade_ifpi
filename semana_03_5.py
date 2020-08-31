b92240304f9e77acf084fb7bf356d74aaa91b05b
daiana sousa <daianasousa04121996@gmail.com>
2020-08-31 11:32:00 -0300

    Crei o arquivo semana_03_5.py

--- semana_03_5.py
+++ semana_03_5.py
@@ -1,6 +1,6 @@
 caractere = str(input('Digite um caractere desejado: ')).lower()
 if caractere in ('a', 'e', 'i', 'o', 'u'):
-  print(vogal)
+  print('vogal')
 
 elif caractere in ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'):
   print('consoante')
